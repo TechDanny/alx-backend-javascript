2e858c6cb310e24575352c04d41a755a438c23c8
root <root@DESKTOP-TKLJENL.localdomain>
2024-01-03 23:11:07 +0300

    block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,7 +1,9 @@
+/* change keywords */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  /* disable */
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
