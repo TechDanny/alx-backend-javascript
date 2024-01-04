53513051fe6329cf3cd6f9c0a4dfcbc283c311a8
root <root@DESKTOP-TKLJENL.localdomain>
2024-01-05 02:20:45 +0300

    pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Code must be a string.');
+    }
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new Error('Name must be a string.');
+    }
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
