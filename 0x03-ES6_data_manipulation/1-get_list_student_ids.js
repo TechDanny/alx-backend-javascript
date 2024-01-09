3c499b2ee2798877e68e09c85d6648e043a349c0
root <kokidanny15@gmail.com>
2024-01-10 00:19:43 +0300

    more mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  const ids = students.map((student) => student.id);
+  return ids;
+}
+export default getListStudentIds;
