65f55f7005b93b911484c0cdda970141c812983c
root <kokidanny15@gmail.com>
2024-01-10 00:01:54 +0300

    Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,20 +1,22 @@
 function getListStudents() {
-    return [
+  return [
     {
-    id: 1,
-    firstName: "Guillaume",
-    location: "San Francisco"
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
     },
+
     {
-    id: 2,
-    firstName: "James",
-    location: "Columbia"
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
     },
+
     {
-    id: 5,
-    firstName: "Serena",
-    location: "San Francisco"
-    }
-]
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+  ];
 }
 export default getListStudents;
