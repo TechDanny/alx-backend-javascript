43d4db41ea434d24ab5351b6cf153411b332a2e2
root <kokidanny15@gmail.com>
2024-03-01 00:06:24 +0300

    Basic test using Chai assertion library

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,23 +1,24 @@
-const calculateNumber = require('./2-calcul');
-const expect = require('expect')
+const calculateNumber = require('./2-calcul_chai');
+const chai = require('chai');
+const expect = chai.expect;
 
 describe('Test sum of float numbers', function() {
   it('Answer is 6', function() {
-    expect(calculateNumber('SUM', 1.4, 4.5)).to.be.equal(6);
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
   });
 });
 describe('Test subtraction of two float numbers', function() {
   it('Answer is -4', function() {
-    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.be.equal(-4);
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
   });
 });
 describe('Test Divide two float numbers', function() {
   it('Answer is 0.2', function() {
-    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.be.equal(0.2);
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
   });
 });
 describe('Test Divide float number with zero', function() {
   it('Answer is ERROR', function() {
-    expect(calculateNumber('DIVIDE', 1.4, 0)).to.be.equal('ERROR');
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('ERROR');
   });
 });
