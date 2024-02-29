033b196dd55e38d807cc54564482700378254c0f
root <kokidanny15@gmail.com>
2024-02-29 23:47:59 +0300

    Basic test using Chai assertion library

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,19 @@
+function calculateNumber(type, a, b) {
+    const arg1 = Math.round(a);
+    const arg2 = Math.round(b);
+
+    if (type === 'SUM') {
+      return arg1 + arg2;
+    }
+    if (type === 'SUBTRACT') {
+      return arg1 - arg2;
+    }
+    if (type === 'DIVIDE') {
+      if (arg2 === 0) {
+        return 'ERROR';
+      } else {
+        return arg1 / arg2;
+      }
+    }
+}
+module.exports = calculateNumber;
