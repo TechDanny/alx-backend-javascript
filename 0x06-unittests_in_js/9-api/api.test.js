073b2e0c57e12ccb2be6f453e7b1bb04dbdaffe7
root <kokidanny15@gmail.com>
2024-03-01 03:38:43 +0300

    Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,35 @@
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Basic Integration testing', () => {
+  describe('home route', () => {
+    it('correct status code', (done) => {
+      const opt = {
+        url: 'http://localhost:7865',
+        method: 'GET',
+      };
+
+      request(opt, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
+    });
+  });
+});
+
+describe('GET /cart/18', () => {
+    it('status code and id 18', (done) => {
+      const opt = {
+        url: 'http://localhost:7865/cart/18',
+        method: 'GET',
+      };
+
+      request(opt, function (error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Payment methods for cart 18');
+        done();
+      });
+    });
+  });
