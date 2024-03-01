0869938aee13d18bf72517242297201d1ecac3e0
root <kokidanny15@gmail.com>
2024-03-01 04:19:41 +0300

    Deep equality & Post integration testing

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,24 @@
+express = require('express');
+const app = express();
+
+app.listen(7865, () => console.log('API available on localhost port 7865'));
+
+app.get('/', (req, res) => {
+    res.end('Welcome to the payment system');
+});
+app.get('/cart/:id([0-9]+)', (req, res) => {
+    res.end(`Payment methods for cart ${req.params.id}`);
+});
+app.get('/available_payments', (_req, res) => {
+    res.json({ payment_methods: { credit_cards: true, paypal: false } });
+});
+app.post('/login', (req, res) => {
+    let username = '';
+  
+    if (req.body) {
+      username = req.body.userName;
+    }
+  
+    res.send(`Welcome ${username}`);
+});
+module.exports = app;
